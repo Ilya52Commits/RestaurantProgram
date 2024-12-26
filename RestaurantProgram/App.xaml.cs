6166ae861f85d4cfbe0e7fc5bb1f5f1a7e3fcf45
krasn <krasnenkov.ilya@inbox.ru>
2024-12-26 19:15:29 +0400

    Доработал DBContext, прописал начальную логику в App.xaml.cs

--- RestaurantProgram/App.xaml.cs
+++ RestaurantProgram/App.xaml.cs
@@ -1,14 +1,42 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using RestaurantProgram.Models;
 using System.Windows;
 
-namespace RestaurantProgram
-{
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
+namespace RestaurantProgram;
 
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    /* Переменная для взаимодействия с базой данных */
+    private readonly DBContext _dbContext = new();
+
+   
+    /// <summary>
+    /// Виртуальный метод для логики перед запуском программы
+    /// </summary>
+    /// <param name="e"></param>
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        var isProductExist = _dbContext.Products.Any();
+
+        if (!isProductExist)
+        {
+            Product product1 = new Product 
+            {
+                Name = "Маэстро бургер",
+                Description = "Острый маэстро бургер", 
+                Сomposition = "Катлета, соус", 
+                Type = "Бургер",
+                Price = 414 
+            };
+
+            _dbContext.Products.Add(product1);
+            _dbContext.SaveChanges();
+        } 
+
+        /* Вызов логики виртульного класса */
+        base.OnStartup(e);
+    }
 }
+
--- RestaurantProgram/Models/DBContext.cs
+++ RestaurantProgram/Models/DBContext.cs
@@ -1,11 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
-namespace RestaurantProgram.Model;
+namespace RestaurantProgram.Models;
 
 internal class DBContext : Microsoft.EntityFrameworkCore.DbContext
 {
+    // строка подключения к базе данных
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
-    optionsBuilder.UseSqlServer(@"Server=(localdb)\\mssqllocaldb;Database=helloappdb;Trusted_Connection=True;");
+        optionsBuilder.UseSqlServer(@"Server=DESKTOP-34SGMAN\LOCALDB;Database=RestaurantProgramDB;Trusted_Connection=True;TrustServerCertificate=True;");
 
-
+    #region Объекты базы данных
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Combo> Combos { get; set; }
+    public DbSet<ChooseProduct> Choices { get; set; }
+    public DbSet<Menu> Menus { get; set; }
+    #endregion
 }

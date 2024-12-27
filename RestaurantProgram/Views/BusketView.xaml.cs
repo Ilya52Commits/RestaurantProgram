47fbb023760c1cd8d59ffa9566819699acfaa70b
krasn <krasnenkov.ilya@inbox.ru>
2024-12-27 22:56:44 +0400

    Допилил "логику" программы

--- RestaurantProgram/Views/BusketView.xaml.cs
+++ RestaurantProgram/Views/BusketView.xaml.cs
@@ -1,16 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using RestaurantProgram.Models;
+using RestaurantProgram.ViewModels;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace RestaurantProgram.View
 {
@@ -19,9 +9,13 @@ namespace RestaurantProgram.View
     /// </summary>
     public partial class BusketView : Window
     {
-        public BusketView()
+        public BusketView(List<Product> selectedProducts)
         {
             InitializeComponent();
+
+            /* Привязка объекта ViewModel к контексту данных */
+            DataContext = new BusketViewModel(selectedProducts);
         }
     }
 }
+    

a4d959d3f9b32242252365fb133208a8dc450dc6
krasn <krasnenkov.ilya@inbox.ru>
2024-12-27 20:58:19 +0400

    Реализовал добавление заказа в коллекцию

--- RestaurantProgram/ViewModels/MenuViewModel.cs
+++ RestaurantProgram/ViewModels/MenuViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CommunityToolkit.Mvvm.Input;
+using Microsoft.EntityFrameworkCore;
 using RestaurantProgram.Models;
 using System.Collections.ObjectModel;
 
@@ -6,8 +7,12 @@ namespace RestaurantProgram.ViewModel;
 internal class MenuViewModel : BaseViewModel
 {
     #region Свойства
-    private readonly DBContext _dbContext; 
+    private readonly DBContext _dbContext;
+
     public ObservableCollection<Menu> Menues { get; set; }
+    private List<Product> _selectedProducts { get; set; }
+
+    public RelayCommand<Product> AddToBascketCommand { get; set; } 
     #endregion
 
     #region Конструкторы
@@ -15,11 +20,17 @@ internal class MenuViewModel : BaseViewModel
     {
         _dbContext = new DBContext();   
         Menues = new ObservableCollection<Menu>(_dbContext.Menus.Include(menu => menu.Prods));
+        _selectedProducts = new List<Product>();
+
+        AddToBascketCommand = new RelayCommand<Product>(AddToBascketCommandExecute); 
     }
     #endregion
 
     #region Методы
-
+    public void AddToBascketCommandExecute(Product product)
+    {
+        _selectedProducts.Add(product);
+    }
     #endregion
 
     #region События
